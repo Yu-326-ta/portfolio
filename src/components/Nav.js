43a8410dc2ecb98a7d83119db9fb0797e42d0ff2
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2024-01-14 17:11:30 +0900

    change services section to skills section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,7 +30,7 @@ const Nav = () => {
             <BiUser />
           </Link>
           <Link
-            to="services"
+            to="skills"
             activeClass="active"
             smooth={true}
             spy={true}
---
+++ src/components/Skills.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { BsArrowUpRight } from "react-icons/bs";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const Services = () => {
+  return (
+    <section className="section" id="skills">
+      <div>skill</div>
+    </section>
+  );
+};
+
+export default Services;
